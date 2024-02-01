76ddb074b6ecdf39a9bdf440f673ba736bf8848b
AzizMdallel <azizmdallel375@gmail.com>
2024-02-01 19:32:48 +0100

    adding

---
+++ props/src/players.jsx
@@ -0,0 +1,58 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://www.google.com/search?gs_ssp=eJzj4tTP1TcwK8xKTzZg9OLJyczPS81RyE0tLs4EAGLjCDU&q=lionel+messi&oq=lionel&gs_lcrp=EgZjaHJvbWUqCggBEC4YsQMYgAQyCQgAEEUYORiABDIKCAEQLhixAxiABDIMCAIQLhgUGIcCGIAEMg0IAxAuGIMBGLEDGIAEMgcIBBAuGIAEMg0IBRAuGIMBGLEDGIAEMgcIBhAuGIAEMgcIBxAAGIAEMgcICBAuGIAEMgcICRAAGI8C0gEIMzI2NmowajSoAgCwAgA&sourceid=chrome&ie=UTF-8#vhid=kICMaDKAWuZ1jM&vssid=l",
+    },
+        {
+          name: "Cristiano Ronaldo",
+          team: "Al NAsser Fc",
+          nationality: "Portugal",
+          jerseyNumber: 7,
+          age: 39,
+          imageUrl: "https://www.google.com/search?q=cristiano+ronaldo&sca_esv=ae2d012acb0463ba&tbm=isch&sxsrf=ACQVn09G2OC8qs_XMkOVw-3piH4vtG1GSQ:1706796060332&source=lnms&sa=X&ved=2ahUKEwjo44TnpoqEAxUtAtsEHXGCAzMQ_AUoAnoECAEQBA&biw=1536&bih=703&dpr=1.25#imgrc=AB35wZltZBgFqM",
+        },
+        {
+            name: "Kevin De Bruyne",
+            team: "Manchester City",
+            nationality: "Belgium",
+            jerseyNumber: 10,
+            age: 37,
+            imageUrl: "https://www.google.com/search?q=kevin+de+bruyne&sca_esv=ae2d012acb0463ba&tbm=isch&sxsrf=ACQVn0_Z_QX3T0aD0nKQv5nkUMwikwu3mQ:1706796157612&source=lnms&sa=X&ved=2ahUKEwiVrbaVp4qEAxXhR_EDHbzzA0AQ_AUoAnoECAEQBA&biw=1536&bih=703&dpr=1.25#imgrc=QZPfM_FkG0h1jM",
+          },
+          {
+            name: "Elyes Achouri",
+            team: "FC Copenhague",
+            nationality: "Tunisia",
+            jerseyNumber: 12,
+            age: 24,
+            imageUrl: "https://www.google.com/search?q=ilyes+achouri&sca_esv=ae2d012acb0463ba&tbm=isch&sxsrf=ACQVn094EAw6BaOl1FeATcDKKye-wMFCrg:1706796438455&source=lnms&sa=X&ved=2ahUKEwi4zKubqIqEAxXpcfEDHRXsCHAQ_AUoAnoECAMQBA&biw=1536&bih=703&dpr=1.25#imgrc=pG_0aZ8u46ktgM",
+          },
+          {
+            name: "Karim Benzema",
+            team: "AL Ettihad",
+            nationality: "France",
+            jerseyNumber: 9,
+            age: 35,
+            imageUrl: "https://www.google.com/search?q=karim+benzema&sca_esv=ae2d012acb0463ba&tbm=isch&sxsrf=ACQVn0_qFqBlMxmzYPNt09iZWK5Zk6-Ouw:1706796836380&source=lnms&sa=X&ved=2ahUKEwjKiYvZqYqEAxWHcvEDHZDYCJoQ_AUoAnoECAMQBA&biw=1536&bih=703&dpr=1.25#imgrc=7vMNyfFqjTieaM",
+          },
+          {
+            name: "Vinicius Junior",
+            team: "Real Madrid",
+            nationality: "Brazil",
+            jerseyNumber: 22,
+            age: 7,
+            imageUrl: "https://www.google.com/search?q=vinicius&sca_esv=ae2d012acb0463ba&tbm=isch&sxsrf=ACQVn08WtkdN3qj9aATO0pm51_e1Pb_sqg:1706796902045&source=lnms&sa=X&ved=2ahUKEwie9rL4qYqEAxVfbvEDHdYWCRoQ_AUoAnoECAIQBA#imgrc=Jkilt7FIhOS3UM",
+          },
+        
+      ];
+      
+      export default players;
+      
+  
+  
+
+  
